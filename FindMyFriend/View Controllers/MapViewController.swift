2466164f19cf98daa28ba8f27c64bf9a8ad8e5d5
Håvard Norén <havardnoren@gmail.com>
2019-06-16 18:32:51 +0200

    ask for location access, center map to user location and show user location on map

--- FindMyFriend/View Controllers/MapViewController.swift
+++ FindMyFriend/View Controllers/MapViewController.swift
@@ -7,24 +7,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
+    private let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        } else {
+            #warning("Handle case where app does not have location access or location service is disabled")
+        }
+        
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = false
+        mapView.showsUserLocation = true
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - CLLocationManagerDelegate
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let center = location.coordinate
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.mapView.setRegion(region, animated: true)
+        }
     }
-    */
-
 }

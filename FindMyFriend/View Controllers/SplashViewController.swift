f06cab70b6562a7973d1a553a315007a52143898
Håvard Norén <havardnoren@gmail.com>
2019-06-16 22:46:16 +0200

    create splash screen as initial vc to handle future auth flow

---
+++ FindMyFriend/View Controllers/SplashViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SplashViewController.swift
+//  FindMyFriend
+//
+//  Created by mac on 6/16/19.
+//  Copyright © 2019 NorensIKT. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        performSegue(withIdentifier: "map", sender: nil)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
